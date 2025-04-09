5d15a1a43f802c066ed16884281e93f80b2bdb88
SanderBirkhol <sanderbirkhol@gmail.com>
2025-04-09 13:04:34 +0200

    Initial commit

---
+++ src/main/java/net/pannekake/testmod/Testmod.java
@@ -0,0 +1,17 @@
+package net.pannekake.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Testmod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
